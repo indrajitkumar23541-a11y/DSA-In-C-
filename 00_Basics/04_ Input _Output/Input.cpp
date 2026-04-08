b8f7fcfa1fa0c20831748b6af594ab384ff69034
Mahalaxmi Hostel <kanvnit67@gmail.com>
2026-04-09 00:35:09 +0530

    Add input example with code

---
+++ 00_Basics/04_ Input _Output/Input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a number :";
+    cin>>a;
+    cout<<"you entered a : "<<a<<endl;
+    int b;
+    cout<<"Enetr another number : ";
+    cin>>b;
+    cout<<"you entered b : "<<b<<endl;
+    cout<<"sum of a and b is : "<<a+b<<endl;
+    cout<<"product of a and b is : "<<a*b<<endl;
+    cout<<"difference of a and b is : "<<a-b<<endl;
+    cout<<"quotient of a and b is : "<<a/b<<endl;
+    return 0;
+}
