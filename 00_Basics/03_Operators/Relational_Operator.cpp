5cae02992aa2b7a81a01419f382b68ad87647c00
Mahalaxmi Hostel <kanvnit67@gmail.com>
2026-04-08 17:28:21 +0530

    Add Relational Operators example with code

---
+++ 00_Basics/03_Operators/Relational_Operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 56, b = 48;
+    cout<<"a > b : "<<(a>b)<<endl; // Grater than operator
+    cout<<"a < b : "<<(a<b)<<endl; // Less than operator
+    cout<<"a >= b : "<<(a>=b)<<endl; // Grater than or equal to operator
+    cout<<"a <= b : "<<(a<=b)<<endl; // Less than or equal to operator
+    cout<<"a == b : "<<(a==b)<<endl; // Equal to operator
+    cout<<"a != b : "<<(a!=b)<<endl; // Not equal to operator
+    return 0;
+}
