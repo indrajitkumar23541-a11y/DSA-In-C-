da91ecd3090acdfee4d9b9b0a0e25f2cebc27274
Mahalaxmi Hostel <kanvnit67@gmail.com>
2026-04-08 15:39:03 +0530

    Add data types code

---
+++ 00_Basics/01_Data_Types/Data_Types_code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age = 25; //Integer data type
+    char grade = 'A'; //Character data type
+    float pi = 3.14; // Float data type
+    double e = 2.71828; // Double data type
+    bool isStudent = true; //Boolean data type
+    cout<<"Age : "<<age<<endl;
+    cout<<"Grade : "<<grade<<endl;
+    cout<<"Pi : "<<pi<<endl;
+    cout<<"Euler 's Number : " <<e <<endl;
+    cout<<"Is Student : "<<isStudent<<endl;
+    return 0;
+
+
+}
