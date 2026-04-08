d61047fd0f585225666fb0ed3a291b937306cfe6
Mahalaxmi Hostel <kanvnit67@gmail.com>
2026-04-08 16:34:30 +0530

    Add code

---
+++ 00_Basics/02_Variables/Variables_Code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 78; // Variable declaration and initialiazation
+    cout<<"Value of x : "<<x<<endl;
+    x = 780; // Variable reassignment
+    cout<<"Value of x after reassignment : "<<x<<endl;
+    int y = 67; // Another variable declaration and initialiazation
+    cout<<"Value of y : "<<y<<endl;
+    cout<<"Sum of x and y : "<<x+y<<endl; // Using variables in an expression
+    return 0;
+}
