d2895579ed5ad4c0b9aa616444440368ba5f7c30
Mahalaxmi Hostel <kanvnit67@gmail.com>
2026-04-08 21:55:39 +0530

    Add Logical Operators Example with code

---
+++ 00_Basics/03_Operators/Logical_Operators.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    bool x = true, y = false;
+    cout<<"x && y : "<<(x&&y)<<endl; // Logical AND operator
+    cout<<"x || y : "<<(x||y)<<endl; // Logiacl OR operator
+    cout<<"!x : "<<(!x)<<endl; // Logiacl NOT operator
+    return 0;
+}
