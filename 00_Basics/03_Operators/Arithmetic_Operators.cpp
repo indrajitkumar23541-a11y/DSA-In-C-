505e60ef5f13050c2e9d769576bf31a6e9c3bd25
Mahalaxmi Hostel <kanvnit67@gmail.com>
2026-04-08 17:09:57 +0530

    Add Arithmetic Operators code

---
+++ 00_Basics/03_Operators/Arithmetic_Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 23, b = 18;
+    cout<<"a+b : "<<a+b<<endl; // Addition operator
+    cout<<"a-b : "<<a-b<<endl; // Subtraction operator
+    cout<<"a*b : "<<a*b<<endl; // Multiplication operator
+    cout<<"a/b : "<<a/b<<endl; // Division operator
+    cout<<"a%b : "<<a%b<<endl; // Modulo operator
+    cout<<"a++ : "<<a++<<endl; // Post-increment opereator
+    cout<<"a-- : "<<a--<<endl; // Post decrement operator
+    cout<<"++b : "<<++b<<endl; // Pre-increment operator
+    cout<<"--b : "<<--b<<endl; // Pre-decrement operator
+    return 0;
+    
+
+}
